54b47fcdd792b7e3e86d164ee9c145bb39c25ca7
AndreLewin <andre.charles.lewin@gmail.com>
2019-04-21 21:13:07 +0200

    feat(p/index): add a transition at todo delete

--- pages/index.vue
+++ pages/index.vue
@@ -36,8 +36,11 @@ export default {
 </script>
 
 <style scoped>
-  .list-complete-enter {
+  .list-complete-enter, .list-complete-leave-to {
     opacity: 0;
     transform: translateY(-30px);
+  }
+  .list-complete-leave-active {
+    position: absolute;
   }
 </style>
