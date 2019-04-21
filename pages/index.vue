23f8a4ddb02eb292d1127f85e0d266e26b1b3249
AndreLewin <andre.charles.lewin@gmail.com>
2019-04-21 16:19:43 +0200

    feat(p/index): display a list a tasks set in data

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,35 @@
     justify-center
     align-center
   >
-    <v-flex>
-      TODOS
-    </v-flex>
+    <v-card
+      v-for="todo in todos"
+      :key="todo.id"  
+    >
+      <v-card-text>
+        {{ todo.text }}
+      </v-card-text>
+    </v-card>
   </v-layout>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      todos: [{
+        id: 1,
+        text: "A simple task",
+        isDone: false
+      }, {
+        id: 2,
+        text: "An usual task",
+        isDone: false
+      }, {
+        id: 3,
+        text: "A rather difficult task",
+        isDone: false
+      }]
+    };
+  }
 }
 </script>
