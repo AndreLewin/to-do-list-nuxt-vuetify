e49d7d33d54d2cb1e644f62f02b37071fee06118
AndreLewin <andre.charles.lewin@gmail.com>
2019-04-22 19:39:59 +0200

    feat(p/index): use draggable to re-order todos

--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,29 @@
     align-center
   >
     <new-todo />
-    <transition-group
-      name="list-complete"
-    >
-      <a-todo
-        v-for="todo in todos"
-        :key="todo.id"
-        :todo="todo"
-      />
-    </transition-group>
+    <draggable>
+      <transition-group
+        name="list-complete"
+      >
+        <a-todo
+          v-for="todo in todos"
+          :key="todo.id"
+          :todo="todo"
+        />
+      </transition-group>
+    </draggable>
   </v-layout>
 </template>
 
 <script>
 import {mapState} from 'vuex';
+import draggable from 'vuedraggable';
 import NewTodo from '~/components/NewTodo';
 import ATodo from '~/components/ATodo';
 
 export default {
   components: {
+    draggable,
     NewTodo,
     ATodo
   },
